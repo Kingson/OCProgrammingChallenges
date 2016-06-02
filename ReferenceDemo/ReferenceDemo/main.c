c3592e58cadadbf6c2d5e2ec9c314b4febfe1ce2
Kingson <xiaomayi0323@gmail.com>
2016-06-02 16:18:28 +0800

    update code

--- ReferenceDemo/ReferenceDemo/main.c
+++ ReferenceDemo/ReferenceDemo/main.c
@@ -13,10 +13,11 @@ void metersToFeetAndInches(double meters, unsigned int *ftPtr, double *inPtr){
     // 这个函数假定meters的值是非负数
     // 将meters变量的值转化为feet的值，类型为浮点数。
     double rawFeet = meters * 3.281; // e.g. 2.4536
+    
     // 计算类型为无符号的整形feet变量的值
     unsigned int feet = (unsigned int)floor(rawFeet);
-    // 将feet变量的值储存在提供的地址里
     
+    // 将feet变量的值储存在提供的地址里
     if (ftPtr) {
         printf("Storing %u to the address %p\n", feet, ftPtr);
         *ftPtr = feet;    // 计算英寸
@@ -24,8 +25,8 @@ void metersToFeetAndInches(double meters, unsigned int *ftPtr, double *inPtr){
     
     double fractionalFoot = rawFeet - feet;
     double inches = fractionalFoot * 12.0;
-    //将inches变量的值保存到传入的地址
     
+    //将inches变量的值保存到传入的地址
     if (inPtr) {
         printf("Storing %.2f to the address %p\n", inches, inPtr);
         *inPtr = inches;
